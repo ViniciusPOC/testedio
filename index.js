ab2f4e1ce9f4dd497759a70ea2f1de550ad27704
ViniciusPOC <viniciu.poc2019@gmail.com>
2024-10-21 22:09:31 -0300

    Alterando para incluir requisitos de comparação

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 // variavel para armazenar nome e xp 
 
 let nomeHeroi = " bob "
-let quantidadeXp = 3000
+let quantidadeXp = 9001
 
 
 let nivelHeroi; 
     {
 if (quantidadeXp < 1000){
     nivelHeroi = "Ferro";
-}   else if (quantidadeXp <=2000) {
+}   else if (quantidadeXp >=1001 && quantidadeXp <=2000) {
     nivelHeroi = "Bronze";
-}   else if (quantidadeXp <=5000) {
+}   else if (quantidadeXp >=2001 && quantidadeXp <=5000) {
     nivelHeroi = "Prata" ; 
-}   else if (quantidadeXp <= 7000) {
+}   else if (quantidadeXp >= 5001 && quantidadeXp<= 7000) {
     nivelHeroi = "Ouro" ; 
-}   else if ( quantidadeXp <=8000) {
+}   else if ( quantidadeXp>= 7001 && quantidadeXp <=8000) {
     nivelHeroi ="Platina"; 
-}   else if (quantidadeXp <= 9000) {
+}   else if (quantidadeXp >= 8001 && quantidadeXp <= 9000) {
     nivelHeroi = "Ascendente" ; 
-}   else if ( quantidadeXp <=10000) {
+}   else if ( quantidadeXp >= 9001 && quantidadeXp <=10000) {
     nivelHeroi ="Imortal"; 
 }   else
     nivelHeroi = "Radiante";
