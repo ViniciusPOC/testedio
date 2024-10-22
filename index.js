8436f904a32696a7b69aeac8bdae76cbb4cac04f
ViniciusPOC <viniciu.poc2019@gmail.com>
2024-10-21 21:46:07 -0300

    Alterando para o codigo do exercicio

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log ("agora sim vamos la");
+// variavel para armazenar nome e xp 
+
+let nomeHeroi = " bob "
+let quantidadeXp = 3000
+
+
+let nivelHeroi; 
+    {
+if (quantidadeXp < 1000){
+    nivelHeroi = "Ferro";
+}   else if (quantidadeXp <=2000) {
+    nivelHeroi = "Bronze";
+}   else if (quantidadeXp <=5000) {
+    nivelHeroi = "Prata" ; 
+}   else if (quantidadeXp <= 7000) {
+    nivelHeroi = "Ouro" ; 
+}   else if ( quantidadeXp <=8000) {
+    nivelHeroi ="Platina"; 
+}   else if (quantidadeXp <= 9000) {
+    nivelHeroi = "Ascendente" ; 
+}   else if ( quantidadeXp <=10000) {
+    nivelHeroi ="Imortal"; 
+}   else
+    nivelHeroi = "Radiante";
+}
+
+// validaçao para impressão 
+
+console.log('O Heroi' +(nomeHeroi)+'esta no nivel ' +(nivelHeroi) )
